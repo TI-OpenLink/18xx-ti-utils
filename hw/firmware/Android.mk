3c70c121620261a4dfbf074d4b6b347fe1015f17
Luciano Coelho <coelho@ti.com>
2012-01-27 15:43:37 +0200

    firmware: change firmware names to new convention
    Change the firmware names to follow the new naming convention, update
    ili file names and Android.mk files.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- hw/firmware/Android.mk
+++ hw/firmware/Android.mk
@@ -10,7 +10,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl127x-fw-mr.bin.r4
+LOCAL_MODULE := wl127x-fw-4-sr.bin
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -18,7 +18,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl127x-fw-mr_plt.bin.r4
+LOCAL_MODULE := wl127x-fw-4-mr.bin
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -26,7 +26,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl128x-fw-mr.bin.r4
+LOCAL_MODULE := wl127x-fw-4-plt.bin
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -34,7 +34,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl128x-fw-mr.bin.r5
+LOCAL_MODULE := wl128x-fw-4-sr.bin
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -42,7 +42,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl128x-fw-mr_plt.bin.r4
+LOCAL_MODULE := wl128x-fw-4-mr.bin
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -50,7 +50,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl128x-fw-mr_plt.bin.r5
+LOCAL_MODULE := wl128x-fw-4-plt.bin
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
