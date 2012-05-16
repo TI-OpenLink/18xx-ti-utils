ce4c49e81725111a1dd1bcc57d8ccfd2033e0d3e
Ido Reis <idor@ti.com>
2012-05-16 17:35:44 +0300

    log proxy utility added
    FW logger over sdio is now being forwarded via socket over adb
    
    Signed-off-by: Ido Reis <idor@ti.com>

--- Android.mk
+++ Android.mk
@@ -23,6 +23,36 @@ LOCAL_MODULE := calibrator
 
 include $(BUILD_EXECUTABLE)
 
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	wl_logproxy.c
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH) \
+	external/libnl-headers
+
+LOCAL_NO_DEFAULT_COMPILER_FLAGS := true
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libnl-headers \
+	$(TARGET_PROJECT_INCLUDES) $(TARGET_C_INCLUDES)
+LOCAL_CFLAGS := $(TARGET_GLOBAL_CFLAGS) $(PRIVATE_ARM_CFLAGS)
+
+LOCAL_CFLAGS += -DCONFIG_LIBNL20=y
+
+LOCAL_MODULE := wl_logproxy
+LOCAL_LDFLAGS := -Wl,--no-gc-sections
+LOCAL_MODULE_TAGS := debug
+#LOCAL_SHARED_LIBRARIES := libnl
+LOCAL_STATIC_LIBRARIES := libnl_2
+LOCAL_MODULE := wl_logproxy
+
+include $(BUILD_EXECUTABLE)
+
+
+
+
+
 include $(LOCAL_PATH)/hw/Android.mk
 
 #
