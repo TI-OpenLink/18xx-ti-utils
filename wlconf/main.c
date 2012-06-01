3bfbcbd03d006477d541e633ba59fde85ddaba34
Luciano Coelho <coelho@ti.com>
2012-06-02 01:11:42 +0300

    wlconf: move some definitions to new wlconf.h
    Some of the definitions belong in a header file.  Create a new
    wlconf.h file and move some of the structure and macro definitions to
    it.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- wlconf/main.c
+++ wlconf/main.c
@@ -26,86 +26,7 @@
 #include <getopt.h>
 
 #include "crc32.h"
-
-struct element {
-	char *name;
-	int type;
-	int array_size;
-	int *value;
-	size_t position;
-};
-
-struct structure {
-	char *name;
-	int n_elements;
-	struct element *elements;
-	size_t size;
-};
-
-struct type {
-	char *name;
-	int size;
-	char *format;
-};
-
-struct dict_entry {
-	char *ini_str;
-	char *element_str;
-};
-
-enum text_file_type {
-	TEXT_FILE_INI,
-	TEXT_FILE_CONF,
-};
-
-struct type types[] = {
-	{ "u32", 4, "%u" },
-	{ "u16", 2, "%u" },
-	{ "u8",  1, "%u" },
-	{ "s32", 4, "%d" },
-	{ "s16", 2, "%d" },
-	{ "s8",  1, "%d" },
-	{ "__le32", 4, "%d" },
-	{ "__le16", 2, "%d" },
-};
-
-#define DEFAULT_INPUT_FILENAME	"wl18xx-conf-default.bin"
-#define DEFAULT_OUTPUT_FILENAME	"wl18xx-conf.bin"
-#define DEFAULT_BIN_FILENAME	"struct.bin"
-#define DEFAULT_DICT_FILENAME	"dictionary.txt"
-#define DEFAULT_ROOT_STRUCT	"wlcore_conf_file"
-#define DEFAULT_MAGIC_SYMBOL	"WL18XX_CONF_MAGIC"
-#define DEFAULT_VERSION_SYMBOL	"WL18XX_CONF_VERSION"
-#define DEFAULT_MAGIC_ELEMENT	"header.magic"
-#define DEFAULT_VERSION_ELEMENT	"header.version"
-#define DEFAULT_CHKSUM_ELEMENT	"header.checksum"
-
-#define STRUCT_BASE		1000
-
-#define STRUCT_PATTERN 	"[\n\t\r ]*struct[\n\t\r ]+([a-zA-Z0-9_]+)"	\
-	"[\n\t\r ]*\\{[\n\t\r ]*([^}]*)\\}[\n\t\r ]*"			\
-	"[a-zA-Z0-9_]*;[\n\t\r ]*"
-
-#define ELEMENT_PATTERN	"[\n\t\r ]*([A-Za-z0-9_]+)[\n\t\r ]+" \
-	"([a-zA-Z_][a-zA-Z0-9_]*)(\\[([0-9]+)\\])?[\n\t\r ]*;[\n\t\r ]*"
-
-#define TEXT_CONF_PATTERN	"^[\t ]*([A-Za-z_][A-Za-z0-9_.]*)" \
-	"[\t ]*=[\t ]*([A-Za-z0-9_, \t]+)"
-
-#define TEXT_INI_PATTERN	"^[\t ]*([A-Za-z_][A-Za-z0-9_]*)" \
-	"[\t ]*=[\t ]*([0-9A-Fa-f \t]+)"
-
-#define DICT_PATTERN		"^[\t ]*([A-Za-z_][A-Za-z0-9_]*)" \
-	"[\t ]+([A-Za-z_][A-Za-z0-9_.]*)"
-
-#define CC_COMMENT_PATTERN	"(([^/]|[^/][^/])*)//[^\n]*\n(.*)"
-
-#define C_COMMENT_PATTERN	"(([^/]|[^/][^*])*)/\\*(([^*]|[^*][^/])*)\\*/(.*)"
-
-
-/* we only match WL12XX and WL18XX magic */
-#define DEFINE_PATTERN	"#define[\n\t\r ]+([A-Za-z_][A-Za-z0-9_]*)"	\
-	"[\n\t\r ]+(0x[0-9A-Fa-f]+)"
+#include "wlconf.h"
 
 static struct dict_entry *dict = NULL;
 static int n_dict_entries = 0;
@@ -752,14 +673,6 @@ out:
 	return ret;
 }
 
-#define WRITE_VAL(val, file) {				\
-		fwrite(&val, 1, sizeof(val), file);	\
-	}
-
-#define READ_VAL(val, file) {				\
-		fread(&val, 1, sizeof(val), file);	\
-	}
-
 static int write_element(FILE *file, struct element *element)
 {
 	size_t name_len = strlen(element->name);
@@ -1036,8 +949,6 @@ out:
 	return ret;
 }
 
-#define MAX_ARRAY_STR_LEN	4096
-#define MAX_VALUE_STR_LEN	256
 static int translate_ini(char **element_str, char **value_array)
 {
 	int i, ret = 0;
