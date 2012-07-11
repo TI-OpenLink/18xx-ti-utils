c730fed12dd530efb1a27fd21ba29614458f4424
Ido Reis <idor@ti.com>
2012-07-11 19:35:30 +0300

    wlconf: update ini files for pg2
    Signed-off-by: Ido Reis <idor@ti.com>
    Signed-off-by: Yair Shapira <yair.shapira@ti.com>

--- wlconf/official_inis/Android.mk
+++ wlconf/official_inis/Android.mk
@@ -5,7 +5,7 @@ LOCAL_PATH:= $(call my-dir)
 etc_target_dir := $(TARGET_OUT)/etc/wifi/wlconf/official_inis
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := WL8_System_parameters_PG2_RDL_1_5_DVP.ini
+LOCAL_SRC_FILES := WL8_System_parameters_PG2_RDL_1_3_HP_SISO.ini
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(etc_target_dir)
@@ -13,7 +13,7 @@ LOCAL_MODULE := $(LOCAL_SRC_FILES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := WL8_System_parameters_PG2_RDL_1_5_HDK.ini
+LOCAL_SRC_FILES := WL8_System_parameters_PG2_RDL_2_4_SP_MIMO.ini
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(etc_target_dir)
@@ -21,17 +21,11 @@ LOCAL_MODULE := $(LOCAL_SRC_FILES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := WL8_System_parameters_PG2_RDL_2_7_DVP.ini
+LOCAL_SRC_FILES := WL8_System_parameters_PG2_RDL_2_4_SP_SISO.ini
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(etc_target_dir)
 LOCAL_MODULE := $(LOCAL_SRC_FILES)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := WL8_System_parameters_PG2_RDL_2_7_HDK.ini
-LOCAL_MODULE_TAGS := debug
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(etc_target_dir)
-LOCAL_MODULE := $(LOCAL_SRC_FILES)
-include $(BUILD_PREBUILT)
+
