214dfbdf97cc390b0020c4ea9331a1cf82240a64
Igal Chernobelsky <igalc@ti.com>
2013-03-06 17:30:40 +0200

    ini: add basic WL8_System_parameters.ini
    Add single basic INI file and remove others.
    testing_set_wlcore.sh script modifies this parameters of basic file
    according to board type and HT mode (hdk/com8 and mimo/siso20/siso40)
    
    Signed-off-by: Igal Chernobelsky <igalc@ti.com>

--- wlconf/official_inis/Android.mk
+++ wlconf/official_inis/Android.mk
@@ -5,27 +5,10 @@ LOCAL_PATH:= $(call my-dir)
 etc_target_dir := $(TARGET_OUT)/etc/wifi/wlconf/official_inis
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := WL8_System_parameters_PG2_RDL_1_3_HP_SISO.ini
+LOCAL_SRC_FILES := WL8_System_parameters.ini
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(etc_target_dir)
 LOCAL_MODULE := $(LOCAL_SRC_FILES)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := WL8_System_parameters_PG2_RDL_2_4_SP_MIMO.ini
-LOCAL_MODULE_TAGS := debug
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(etc_target_dir)
-LOCAL_MODULE := $(LOCAL_SRC_FILES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := WL8_System_parameters_PG2_RDL_2_4_SP_SISO.ini
-LOCAL_MODULE_TAGS := debug
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(etc_target_dir)
-LOCAL_MODULE := $(LOCAL_SRC_FILES)
-include $(BUILD_PREBUILT)
-
 
